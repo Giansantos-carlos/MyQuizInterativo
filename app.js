64acf38fda14a78881558a3726fa777b915519f9
Gian Carlos Dos Santos <giansantos.carlos@gmail.com>
2022-03-31 20:19:10 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const correctAnswers =  ['B','D','C','B','A','C','A','C','D','A']
+const form = document.querySelector('.quiz-form')
+const questions = document.querySelectorAll('.my-5')
+const FinalScoreContaneir =  document.querySelector('.final-score-contanier')
+let counter = 0
+let score = 0
+const getUsersAnswers = () => {
+    let usersAnswers = []
+    correctAnswers.forEach((correctAnswer, index) => {
+        const userAnswer = form[`inputQuestion${index + 1}`].value
+        usersAnswers.push(userAnswer)
+    })     
+    return usersAnswers
+}
+const calculateUserScore = usersAnswers => {
+    usersAnswers.forEach((usersAnswer, index)=> {
+        const isUserAnswerCorrect = usersAnswer === correctAnswers[index]
+        let colorAnswerCorrect = questions[index + 1].style.color
+        if(isUserAnswerCorrect){
+            score += 10
+            colorAnswerCorrect = "#00FF00"
+            return
+        }
+        colorAnswerCorrect = "#FF0000"
+    })
+}
+const showFinalScore = () => {
+    scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+    FinalScoreContaneir.classList.remove('d-none')
+} 
+const animateFinalScore = () => {
+    const timer = setInterval(() => {
+        if(counter === score){
+            clearInterval(timer)
+        }
+        FinalScoreContaneir.querySelector('span').textContent = `${counter++}%`
+    }, 10)
+}
+form.addEventListener('submit', event  => {
+    event.preventDefault()
+    const usersAnswers = getUsersAnswers()   
+    calculateUserScore(usersAnswers)
+    showFinalScore()
+    animateFinalScore()
+})
