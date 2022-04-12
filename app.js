759a7672817166a0864f5c1312cf10f85fa79dc4
Gian Carlos Dos Santos <giansantos.carlos@gmail.com>
2022-04-11 21:34:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const correctAnswers =  ['B','D','C','B','A','C','A','C','D','A']
 const form = document.querySelector('.quiz-form')
 const questions = document.querySelectorAll('.my-5')
-const FinalScoreContaneir =  document.querySelector('.final-score-contanier')
-let counter = 0
+const finalScoreContaneir =  document.querySelector('.final-score-contanier')
+
 let score = 0
-const getUsersAnswers = () => {
-    let usersAnswers = []
-    correctAnswers.forEach((correctAnswer, index) => {
-        const userAnswer = form[`inputQuestion${index + 1}`].value
-        usersAnswers.push(userAnswer)
-    })     
-    return usersAnswers
-}
-const calculateUserScore = usersAnswers => {
+const getUsersAnswers = () => correctAnswers.map((_, index) => 
+ form[`inputQuestion${index + 1}`].value)
+
+ const calculateUserScore = usersAnswers => {
     usersAnswers.forEach((usersAnswer, index)=> {
         const isUserAnswerCorrect = usersAnswer === correctAnswers[index]
         let colorAnswerCorrect = questions[index + 1].style.color
@@ -30,19 +25,31 @@ const showFinalScore = () => {
         left: 0,
         behavior: 'smooth'
     })
-    FinalScoreContaneir.classList.remove('d-none')
+    finalScoreContaneir.classList.remove('d-none')
 } 
+
+
 const animateFinalScore = () => {
+
+    let counter = 0
     const timer = setInterval(() => {
         if(counter === score){
             clearInterval(timer)
         }
-        FinalScoreContaneir.querySelector('span').textContent = `${counter++}%`
+
+        finalScoreContaneir.querySelector('span').textContent = `${counter++}%`
     }, 10)
 }
+
+const resetUserScore = () => {
+    score = 0
+}
+
 form.addEventListener('submit', event  => {
     event.preventDefault()
-    const usersAnswers = getUsersAnswers()   
+    const usersAnswers = getUsersAnswers()
+    
+    resetUserScore()
     calculateUserScore(usersAnswers)
     showFinalScore()
     animateFinalScore()
